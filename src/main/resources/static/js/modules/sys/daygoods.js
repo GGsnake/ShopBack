7ab2a202485d324eba3258c3bc041057705f0ea2
GGsnake <ljp462693266@outlook.com>
2019-01-04 21:11:19 +0800

    后台页面完善

---
+++ src/main/resources/static/js/modules/sys/daygoods.js
@@ -0,0 +1,126 @@
+$(function () {
+    $("#jqGrid").jqGrid({
+        url: baseURL + 'sys/daygoods/list',
+        datatype: "json",
+        colModel: [			
+			{ label: 'id', name: 'id', index: 'id', width: 50, key: true },
+			{ label: '标题', name: 'titile', index: 'titile', width: 80 }, 			
+			{ label: '内容', name: 'content', index: 'content', width: 80 }, 			
+			{ label: '头像', name: 'image', index: 'image', width: 80 }, 			
+			{ label: '图片内容url数组', name: 'contentImages', index: 'content_images', width: 80 }, 			
+			{ label: '删除状态', name: 'status', index: 'status', width: 80 }, 			
+			{ label: '创建时间', name: 'createtime', index: 'createTime', width: 80 }			
+        ],
+		viewrecords: true,
+        height: "100%",
+        rowNum: 10,
+		rowList : [10,30,50],
+        rownumbers: true, 
+        rownumWidth: 25, 
+        autowidth:true,
+        multiselect: true,
+        pager: "#jqGridPager",
+        jsonReader : {
+            root: "page.list",
+            page: "page.currPage",
+            total: "page.totalPage",
+            records: "page.totalCount"
+        },
+        prmNames : {
+            page:"page", 
+            rows:"limit", 
+            order: "order"
+        },
+        gridComplete:function(){
+        	//隐藏grid底部滚动条
+        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" }); 
+        }
+    });
+});
+
+var vm = new Vue({
+	el:'#rapp',
+	data:{
+		showList: true,
+		title: null,
+		daygoods: {}
+	},
+	methods: {
+		query: function () {
+			vm.reload();
+		},
+		add: function(){
+			vm.showList = false;
+			vm.title = "新增";
+			vm.daygoods = {};
+		},
+		update: function () {
+			var id = getSelectedRow();
+			if(id == null){
+				return ;
+			}
+			vm.showList = false;
+            vm.title = "修改";
+            
+            vm.getInfo(id)
+		},
+		saveOrUpdate: function () {
+			var url = vm.daygoods.id == null ? "sys/daygoods/save" : "sys/daygoods/update";
+			$.ajax({
+				type: "POST",
+			    url: baseURL + url,
+                contentType: "application/json",
+			    data: JSON.stringify(vm.daygoods),
+			    success: function(r){
+			    	if(r.code === 0){
+						alert('操作成功', function(){
+							vm.reload();
+						});
+					}else{
+						alert(r.msg);
+					}
+				}
+			});
+		},
+		del: function () {
+			var ids = getSelectedRows();
+			if(ids == null){
+				return ;
+			}
+			
+			confirm('确定要删除选中的记录？', function(){
+				$.ajax({
+					type: "POST",
+				    url: baseURL + "sys/daygoods/delete",
+                    contentType: "application/json",
+				    data: JSON.stringify(ids),
+				    success: function(r){
+						if(r.code == 0){
+							alert('操作成功', function(){
+                                vm.reload();
+							});
+						}else{
+							alert(r.msg);
+						}
+					}
+				});
+			});
+		},
+		getInfo: function(id){
+			$.get(baseURL + "sys/daygoods/info/"+id, function(r){
+                vm.daygoods = r.daygoods;
+            });
+		},
+		reload: function () {
+			vm.showList = true;
+			var page = $("#jqGrid").jqGrid('getGridParam','page');
+			$("#jqGrid").jqGrid('setGridParam',{ 
+                page:page
+            }).trigger("reloadGrid");
+		},
+        refresh: function () {
+            vm.showList = true;
+            window.location.reload();
+        }
+	}
+});
